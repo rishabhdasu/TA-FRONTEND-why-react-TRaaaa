22045ded5de7c84f6ca05b5161f9c9ed9e1b02f5
Rishabh Dasgupta <rishabhdasu@gmail.com>
2022-05-13 10:53:36 +0530

    add

---
+++ block-BRaaac/code/script.js
@@ -0,0 +1,64 @@
+let input = document.querySelector("input");
+let root = document.querySelector(".root");
+
+// Data
+
+let movies = [
+  { name: "Dunkirk", watched: false },
+  { name: "Inception", watched: true },
+];
+
+function elm(type, attr = {}, ...children) {
+  let element = document.createElement(type);
+  for (let key in attr) {
+    if (key.startsWith("data-")) {
+      element.setAttribute(key, attr[key]);
+    } else {
+      element[key] = attr[key];
+    }
+  }
+  children.forEach((child) => {
+    if (typeof child === "object") {
+      element.append(child);
+    }
+    if (typeof child === "string") {
+      let node = document.createTextNode(child);
+      element.append(node);
+    }
+  });
+  return element;
+}
+
+function handleChange(event) {
+  let id = event.target.id;
+  movies[id].watched = !movies[id].watched;
+  createUI(movies);
+}
+
+function handleEvent(event) {
+  if (event.keyCode === 13) {
+    movies.push({
+      name: event.target.value,
+      watched: false,
+    });
+    event.target.value = "";
+    createUI(movies);
+  }
+}
+
+function createUI(data) {
+  root.innerHTML = "";
+  data.forEach((movie, index) => {
+    let li = elm(
+      "li",
+      { class: "flex" },
+      elm("label", { for: index }, movie.name),
+      elm("button", { id: index, addEventListener: "click" })
+    );
+
+    root.append(li);
+  });
+}
+
+createUI(movies);
+input.addEventListener(`keyup`, handleEvent);
